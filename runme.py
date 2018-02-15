c5e57a82b654f25953cb07e62c3802f71f9b9fa6
Max Z <max.zlotskiy@gmail.com>
2018-02-14 22:59:22 -0500

    made score criteria even bigger now

--- runme.py
+++ runme.py
@@ -52,8 +52,8 @@ print ""
 print "The one with an A is"
 print get_by_zipcode_and_grade("10282", "A")[:1]
 print ""
-print "The one with a score less than 10 is"
-print get_by_zipcode_and_score("10282", "10")[:1]
+print "The one with a score less than 100 is"
+print get_by_zipcode_and_score("10282", "100")[:1]
 print ""
 print "Uhg, not this slop..."
 print interesting("Brooklyn", "Russian")[:1]
